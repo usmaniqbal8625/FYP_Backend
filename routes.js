bdf0fd38e263c66978f7ce16cb6bbf35046146c4
usmaniqbal8625 <usmaniqbal8625@gmail.com>
2025-09-02 04:57:41 +0500

    ok

--- routes.js
+++ routes.js
@@ -1,10 +1,10 @@
 "use strict";
 
 module.exports = (app) => {
-  app.use("/v1/auth", require("./api/auth"));
-  app.use("/v1/users", require("./api/user"));
-  app.use("/v1/laptop", require("./api/laptop"));
-  app.use("/v1/review", require("./api/review"));
-  app.use("/v1/cart", require("./api/cart"));
-  app.use("/v1/complaint", require("./api/complaint"));
+  app.use("/auth", require("./api/auth"));
+  app.use("/users", require("./api/user"));
+  app.use("/laptop", require("./api/laptop"));
+  app.use("/review", require("./api/review"));
+  app.use("/cart", require("./api/cart"));
+  app.use("/complaint", require("./api/complaint"));
 };
